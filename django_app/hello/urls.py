e6ebe7ea99f7fbecd5a0317d82cd265531e4622e
森田賢二 <kenjimoritata@gmail.com>
2018-08-15 10:17:04 +0900

    add another page

--- django_app/hello/urls.py
+++ django_app/hello/urls.py
@@ -3,4 +3,5 @@ from . import views
 # http://127.0.0.1:8000/hello/my_name_is_taromorita.I_am_38_years_old.
 urlpatterns = [
     path('', views.index, name='index'),
+    path('next', views.next, name='next'),
 ]
