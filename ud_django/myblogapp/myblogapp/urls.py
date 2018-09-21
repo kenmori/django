fe4d2a05129e34e88e7a3f63828a2ddd89930de8
森田賢二 <kenjimoritata@gmail.com>
2018-09-22 08:47:29 +0900

    add

--- ud_django/myblogapp/myblogapp/urls.py
+++ ud_django/myblogapp/myblogapp/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from posts import views
 
 urlpatterns = [
     path('posts/', include(('posts.urls','index'),)),
     path('admin/', admin.site.urls),
+    path('<int:post_id>/', include(('posts.views.post_detail','post_detail'),))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
