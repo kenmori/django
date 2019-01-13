76f23cddc6d53e73d4403e32917e8cffd0d3f937
kenjimorita <>
2019-01-13 13:13:08 -0800

    35

--- django_study/myblogapp/posts/views.py
+++ django_study/myblogapp/posts/views.py
@@ -11,4 +11,5 @@ def index(request):
 
 
 def post_detail(request, post_id):
-    return render(request, "posts/post_detail.html", {'post_id': post_id})
+    post = Post.objects.get(pk=post_id)
+    return render(request, "posts/post_detail.html", {'post': post})
